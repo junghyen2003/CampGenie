1e211bf549f2a5146c3ededca6c9b37eb392d996
조중현 <junghyun@aprd.io>
2022-05-31 13:05:17 +0900

    init project

---
+++ src/main/java/com/lonikan/campgenie/CampGenieApplication.java
@@ -0,0 +1,13 @@
+package com.lonikan.campgenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampGenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampGenieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lonikan/campgenie/CampGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lonikan.campgenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampGenieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
